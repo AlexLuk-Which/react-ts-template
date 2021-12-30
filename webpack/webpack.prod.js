73cc2aeb56870a5df6c0dd5b561dadb6f77d32c0
Alex Luk <alexcbluk@hotmail.com>
2021-12-30 18:20:57 +0000

    DEV-000 Setup Webpack + sass loader + hot module reload + eslintrc

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new Dotenv({
+      path: '.env.production'
+    })
+  ],
+}
